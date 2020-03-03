27d69ea7be326e456923bbaf8471f423af90915f
Akshat <ak127a@gmail.com>
2020-03-03 21:14:53 +0530

    navbar changes(consider auth state)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,83 @@
 import React, { Component } from "react";
 import logoonly from "../images/logoonly.png";
 import textonly from "../images/textonly.png";
+import PropTypes from "prop-types";
 
 // MUI stuff
 import AppBar from "@material-ui/core/AppBar";
+import Tooltip from "@material-ui/core/Tooltip";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import HomeIcon from "@material-ui/icons/Home";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import AddIcon from "@material-ui/icons/Add";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 
+// REDUX
+import { connect } from "react-redux";
+
 const styles = {
-  navbarbuttons: { margin: 0, width: "100vw", textAlign: "right" }
+  navbarbuttons: { position: "absolute", right: "30px" },
+  centredButtons: {
+    width: "150px",
+    position: "absolute",
+    marginLeft: "-75px",
+    left: "50%"
+  }
 };
 
 class Navbar extends Component {
   render() {
+    const { authenticated } = this.props;
     const { classes } = this.props;
     return (
       <AppBar>
         <Toolbar>
           <img src={logoonly} alt="logo" id="navbar-logo" />
           <img src={textonly} alt="logo" id="navbar-logo-text" />
+          {authenticated && (
+            <React.Fragment>
+              <div className={classes.centredButtons}>
+                <Tooltip placement="bottom" title="Add new post">
+                  <IconButton>
+                    <AddIcon style={{ color: "#fff" }} />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip placement="bottom" title="Home">
+                  <IconButton component={Link} to="/">
+                    <HomeIcon style={{ color: "#fff" }} />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip placement="bottom" title="Notifications">
+                  <IconButton>
+                    <NotificationsIcon style={{ color: "#fff" }} />
+                  </IconButton>
+                </Tooltip>
+              </div>
+              <div className={classes.navbarbuttons}>
+                <Button startIcon={<ExitToAppIcon />} color="inherit">
+                  Logout
+                </Button>
+              </div>
+            </React.Fragment>
+          )}
           <div className={classes.navbarbuttons}>
-            <Button color="inherit" component={Link} to="/login">
-              Login
-            </Button>
-            <Button color="inherit" component={Link} to="/">
-              Home
-            </Button>
-            <Button color="inherit" component={Link} to="/signup">
-              SignUp
-            </Button>
+            {!authenticated && (
+              <React.Fragment>
+                <Button color="inherit" component={Link} to="/login">
+                  Login
+                </Button>
+                <Button color="inherit" component={Link} to="/">
+                  Home
+                </Button>
+                <Button color="inherit" component={Link} to="/signup">
+                  SignUp
+                </Button>
+              </React.Fragment>
+            )}
           </div>
         </Toolbar>
       </AppBar>
@@ -38,4 +85,12 @@ class Navbar extends Component {
   }
 }
 
-export default withStyles(styles)(Navbar);
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps, null)(withStyles(styles)(Navbar));
--- src/redux/types.js
+++ src/redux/types.js
@@ -6,9 +6,14 @@ export const LOADING_USER = "LOADING_USER";
 // Ui reducer types
 export const SET_ERRORS = "SET_ERRORS";
 export const LOADING_UI = "LOADING_UI";
+export const LOADING_DATA = "LOADING_DATA";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SHOW_SIGNUP_SUCCESS = "SHOW_SIGNUP_SUCCESS";
 export const CLOSE_SIGNUP_SUCCESS = "CLOSE_SIGNUP_SUCCESS";
 export const SHOW_LOGIN_SUCCESS = "SHOW_LOGIN_SUCCESS";
 export const CLOSE_LOGIN_SUCCESS = "CLOSE_LOGIN_SUCCESS";
 // Data reducer types
+export const SET_SCREAMS = "SET_SCREAMS";
+export const SET_SCREAM = "SET_SCREAM";
+export const LIKE_SCREAMS = "LIKE_SCREAMS";
+export const UNLIKE_SCREAMS = "UNLIKE_SCREAMS";
