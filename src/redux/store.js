99ce0214ba9f2239b6007e30f0ff75fc035a602c
Akshat <ak127a@gmail.com>
2020-03-13 19:42:31 +0530

    fixed syntax error in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,8 +19,10 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-  )
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
 
